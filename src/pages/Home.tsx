176d942df9e4a03a876bc790163e58d662118436
webspmb <absensi.sdnkajulangko@gmail.com>
2026-04-28 09:14:08 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -58,7 +58,7 @@ export default function Home() {
               className="text-5xl md:text-6xl font-extrabold text-slate-900 tracking-tight leading-tight mb-6"
             >
               Wujudkan Masa Depan <br className="hidden md:block" />
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-400">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-500">
                 Anak Bangsa Yang Gemilang
               </span>
             </motion.h1>
